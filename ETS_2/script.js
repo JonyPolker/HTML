9b183e07b440ca296cdc3da3e0bf19b9c12a5425
Sergey <s.efimov1904@gmail.com>
2025-03-05 11:58:32 +0300

    Add files via upload

---
+++ ETS_2/script.js
@@ -0,0 +1,21 @@
+function setTheme(theme) {
+    document.body.className = theme; // Примените тему к body
+    localStorage.setItem('theme', theme); // Сохраните тему в локальном хранилище
+}
+
+function loadTheme() {
+    const savedTheme = localStorage.getItem('theme'); // Получите сохранённую тему
+    if (savedTheme) {
+        setTheme(savedTheme); // Примените сохранённую тему
+    } else {
+        setTheme('light'); // Установите тему по умолчанию
+    }
+}
+
+document.getElementById('theme-toggle').onclick = function() {
+    const currentTheme = localStorage.getItem('theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+};
+
+loadTheme(); // Загрузите тему при инициализации страницы
